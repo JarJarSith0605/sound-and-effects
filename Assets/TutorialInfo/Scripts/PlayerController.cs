e53a3215a1fd1eb5eda02eae19ca97f25f0c242e
Zachary <159571194+JarJarSith0605@users.noreply.github.com>
2025-05-15 10:11:22 -0700

    character jumps

---
+++ Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    public float jumpForce = 3;
+
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {            
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+}
+}
