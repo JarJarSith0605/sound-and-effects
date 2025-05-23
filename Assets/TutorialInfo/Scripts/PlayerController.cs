2a911473f27ffbca0fb6437afab54eccc3936a9e
Zachary <159571194+JarJarSith0605@users.noreply.github.com>
2025-05-23 10:33:54 -0700

    fixed bugs and added animations

--- Assets/TutorialInfo/Scripts/PlayerController.cs
+++ Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -6,22 +6,28 @@ public class PlayerController : MonoBehaviour
 
     public float jumpForce = 3;
 
+    public float gravityModifier;
+
     public bool isOnGround = true; 
+
+    private Animator playerAnim;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        
+        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameover)
         {            
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -33,6 +39,8 @@ public bool gameover = false;
     }   else if (collision.gameObject.CompareTag("obstacle")) {
         gameover = true;
         Debug.Log("Game Over!");
+        playerAnim.SetBool("Death_b", true);
+        playerAnim.SetInteger("DeathType_int", 1);
     }
 
 }
--- Assets/TutorialInfo/Scripts/moveleft.cs
+++ Assets/TutorialInfo/Scripts/moveleft.cs
@@ -5,17 +5,19 @@ public class moveleft : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerControllerScript =
+        PlayerControllerScript =
         GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    public float speed = 30;
-    private PlayerController playerControllerScript;
+    private float speed = 30;
+    private PlayerController PlayerControllerScript;
+    private float leftBound = -15;
 
     // Update is called once per frame
     void Update()
     {
-        if (playerControllerScript.gameOver == false) {
+        if (PlayerControllerScript.gameover == false) {
             transform.Translate(Vector3.left * Time.deltaTime * speed);}
-    }
-}
+        
+        if (transform.position.x < leftBound && gameObject.CompareTag("obstacle")){
+            Destroy (gameObject); }}}
